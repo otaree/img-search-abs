b85d278d202a823d84079a2f170e1757ae5bae87
otaree <deep.yen.ur.hash@gmail.com>
2018-02-23 08:06:45 +0530

    Edited config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env = 'development') {
+if (env === 'development') {
     process.env.MONGODB_URI = 'mongodb://localhost/search-img';
-} else if (env = 'test') {
+} else if (env === 'test') {
     process.env.MONGODB_URI = 'mongodb://localhost/search-img-test';
 }
